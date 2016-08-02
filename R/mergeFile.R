a74d92ec315f5430813415131fc5f04f639009ee
Geyi Liu <g.liu@alumni.ubc.ca>
2016-08-02 15:04:19 -0700

    txt working

--- R/mergeFile.R
+++ R/mergeFile.R
@@ -14,7 +14,7 @@ mergeFile<-function(){
   # }
   setwd(dirFiles)
   myFiles<-list.files()
-  fileType<-readline(prompt="Enter 1 if all files are xlsx, 2 if all files are csv, or 3 if all files are vcf")
+  fileType<-readline(prompt="Enter 1 if all files are xlsx, 2 if all files are csv, or 3 if all files are txt")
   rowToStart<-readline(prompt="Please enter the first row of data in all data files: ")
   for(i in 1:length(myFiles)){
     if(as.character(fileType)=="2"){
@@ -22,7 +22,7 @@ mergeFile<-function(){
     }else if(as.character(fileType)=="1"){
       tempData<-read.xlsx(myFiles[i],sheetIndex=1,startRow=rowToStart,header=FALSE)
     }else{
-      print("Call Johnson")
+      tempData<-read.table(myFiles[i],skip=as.numeric(rowToStart)-1,header=FALSE)
     }
     tempData$name<-rep(strsplit(myFiles[i],split="[.]")[[1]][1],nrow(tempData))
     allRawData<-rbind(allRawData,tempData)
