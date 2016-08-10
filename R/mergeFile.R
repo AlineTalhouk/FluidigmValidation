e25be18852912a25b1156933734dfca17d2ada69
Geyi Liu <g.liu@alumni.ubc.ca>
2016-08-10 14:55:40 -0700

    no need to enter rowtostart

--- R/mergeFile.R
+++ R/mergeFile.R
@@ -15,15 +15,16 @@ mergeFile<-function(){
   setwd(dirFiles)
   myFiles<-list.files()
   fileType<-readline(prompt="Enter 1 if all files are xlsx, 2 if all files are csv, or 3 if all files are txt: ")
-  rowToStart<-readline(prompt="Please enter the first row of data in all data files: ")
+  #rowToStart<-readline(prompt="Please enter the first row of data in all data files: ")
   for(i in 1:length(myFiles)){
     if(as.character(fileType)=="2"){
-      tempData<-read.csv(myFiles[i],skip=as.numeric(rowToStart)-1,header=FALSE)
+      tempData<-read.csv(myFiles[i],header=T)
     }else if(as.character(fileType)=="1"){
-      tempData<-read.xlsx(myFiles[i],sheetIndex=1,startRow=rowToStart,header=FALSE)
+      tempData<-read.xlsx(myFiles[i],sheetIndex=1,header=T)
     }else{
-      tempData<-read.table(myFiles[i],skip=as.numeric(rowToStart)-1,header=FALSE)
+      tempData<-read.table(myFiles[i],header=T)
     }
+    names(tempData)<-c("#CHROM","POS","ID","REF","ALT","QUAL","FILTER","INFO","FORMAT","NUMS")
     tempData$name<-rep(strsplit(myFiles[i],split="[.]")[[1]][1],nrow(tempData))
     allRawData<-rbind(allRawData,tempData)
   }
