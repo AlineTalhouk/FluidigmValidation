a596292e5feb6fdf6c07aa21f81ea01b870c1a46
Geyi Liu <g.liu@alumni.ubc.ca>
2016-09-12 17:45:51 -0700

    fixed case 5 not appending problem

--- R/labelMutationGroup.R
+++ R/labelMutationGroup.R
@@ -11,6 +11,7 @@ labelMutationGroup<-function(data){
   chunk<-NULL
   mutationGroup<-NULL
   for(i in 1:length(allPos)){
+    #print(i)
     tempGroup<-NULL
     toAdd<-NULL
     chunk<-data[data$POS==allPos[i],]
@@ -18,46 +19,60 @@ labelMutationGroup<-function(data){
     if(notMutationGroupException(tempGroup)){
       if(twiceT1(tempGroup) && !twiceT2(tempGroup) && !onceT2(tempGroup) && !onceN(tempGroup)){
         mutationGroup<-append(mutationGroup,rep("Somatic",length(tempGroup)))
+        #print("case 1")
       }else if(!twiceT1(tempGroup) && !onceT1(tempGroup) && twiceT2(tempGroup) && !onceN(tempGroup)){
         mutationGroup<-append(mutationGroup,rep("Somatic",length(tempGroup)))
+       #print("case 2")
       }else if(onceT1(tempGroup) && onceT2(tempGroup) && !onceN(tempGroup)){
         mutationGroup<-append(mutationGroup,rep("Artifact",length(tempGroup)))
+        #print("case 3")
       }else if(onceT1(tempGroup) && twiceT2(tempGroup) && !onceN(tempGroup)){
         toAdd<-rep("NA",length(tempGroup))
         toAdd[which(tempGroup=="T2")]<-"Somatic"
         toAdd[which(tempGroup=="T1")]<-"Artifact"
         mutationGroup<-append(mutationGroup,toAdd)
+        #print("case 4")
       }else if(onceT1(tempGroup) && onceT2(tempGroup) && onceN(tempGroup)){
-        mutationGroup<-rep("Artifact",length(tempGroup))
+        mutationGroup<-append(mutationGroup,rep("Artifact",length(tempGroup)))
+        #print("case 5")
       }else if(onceT1(tempGroup) && twiceT2(tempGroup) && onceN(tempGroup)){
         toAdd<-rep("NA",length(tempGroup))
         toAdd[which(tempGroup=="T2")]<-"Germline"
         toAdd[which(tempGroup=="T1")]<-"Artifact"
         mutationGroup<-append(mutationGroup,toAdd)
+        #print("case 6")
       }else if(onceT1(tempGroup) && !onceT2(tempGroup) && !twiceT2(tempGroup) && onceN(tempGroup)){
         mutationGroup<-append(mutationGroup,rep("Artifact",length(tempGroup)))
+        #print("case 7")
       }else if(twiceT1(tempGroup) && onceT2(tempGroup) && !onceN(tempGroup)){
         toAdd<-rep("NA",length(tempGroup))
         toAdd[which(tempGroup=="T1")]<-"Somatic"
         toAdd[which(tempGroup=="T2")]<-"Artifact"
         mutationGroup<-append(mutationGroup,toAdd)
+        #print("case 8")
       }else if(twiceT1(tempGroup) && twiceT2(tempGroup) && !onceN(tempGroup)){
         mutationGroup<-append(mutationGroup,rep("Somatic",length(tempGroup)))
+        #print("case 9")
       }else if(twiceT1(tempGroup) && onceT2(tempGroup) && onceN(tempGroup)){
         toAdd<-rep("NA",length(tempGroup))
         toAdd[which(tempGroup=="T1")]<-"Germline"
         toAdd[which(tempGroup)=="T2"]<-"Artifact"
+        mutationGroup<-append(mutationGroup,toAdd)
+        #print("case 10")
       }else if(twiceT1(tempGroup) && twiceT2(tempGroup) && onceN(tempGroup)){
         mutationGroup<-append(mutationGroup,rep("Germline",length(tempGroup)))
+        #print("case 11")
       }else if(twiceT1(tempGroup) && !onceT2(tempGroup) && !twiceT2(tempGroup) && onceN(tempGroup)){
         mutationGroup<-append(mutationGroup,rep("Germline",length(tempGroup)))
       }else if(!onceT1(tempGroup) && !twiceT1(tempGroup) && onceT2(tempGroup) && onceN(tempGroup)){
         mutationGroup<-append(mutationGroup,rep("Artifact",length(tempGroup)))
+        #print("case 12")
       }else if(!onceT1(tempGroup) && !twiceT1(tempGroup) && twiceT2(tempGroup) && onceN(tempGroup)){
         mutationGroup<-append(mutationGroup,rep("Germline",length(tempGroup)))
+        #print("case 13")
       }else{
         mutationGroup<-append(mutationGroup,rep("Attention",length(tempGroup)))
-        print(paste("Attention please: ",getPatientID(as.character(data[1,1]))))
+        #print(paste("Attention please: ",getPatientID(as.character(data[1,1]))))
       }
     }else{
       mutationGroup<-append(mutationGroup,rep("Attention",length(tempGroup)))
--- R/notMutationGroupException.R
+++ R/notMutationGroupException.R
@@ -1,3 +1,11 @@
+#' Title function to check if a chunk is an mutation group exception (e.g have more than 1 N, or more than 2 T1 or T2)
+#'
+#' @param vec : input vector for tumor type
+#'
+#' @return TRUE is it is exception false otherwise
+#' @export
+#'
+#' @examples
 notMutationGroupException<-function(vec){
   if(sum(vec=="N")>1){
     return(FALSE)
